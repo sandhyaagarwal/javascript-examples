93525fafa2833b9b794c9335fecd0d045fd5ac73
Sandhya Agarwal <sandhyaagarwal@users.noreply.github.com>
2017-08-07 18:11:58 +0530

    Create LinkedListTest.js

---
+++ LinkedListTest.js
@@ -0,0 +1,18 @@
+var l = new LinkedList();
+l.add(2);
+l.add(3);
+l.add(4);
+l.add(5);
+l.print();
+
+
+l.remove(2);
+l.print();
+l.remove(3);
+l.print();
+
+l.remove(4);
+l.print();
+
+l.remove(5);
+l.print();
